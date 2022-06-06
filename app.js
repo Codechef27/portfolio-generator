2a95d8846624403a79bedc183fb46e24eb57b78c
BradleyK <bradleyjkennett@gmail.com>
2022-06-06 18:07:14 -0400

    generate page add

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const FileSystem = require('fs');
+
+const generatePage = require('./src/page-template');
+
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1){
-    console.log(profileDataArr[i]);
-    }
+FileSystem.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    console.log('=================');
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData (profileDataArgs);
+// printProfileData (profileDataArgs);
